45b0c9fdf4675cf1dd104b00a51c3d2cedcdf883
Westindiess <gregoryalexiss@gmail.com>
2022-01-04 10:44:49 +0100

    update

--- src/components/GradientCard/GradientCard.tsx
+++ src/components/GradientCard/GradientCard.tsx
@@ -16,9 +16,12 @@ const GradientCard: React.FC = () => {
   const filter = useSelector((state: RootState) => state.filterTag.tags);
   const list = filterByTag(data, filter, "All");
   return (
-    <ul className="gap-3 sm:grid sm:grid-cols-2 xl:grid-cols-4 mt-5">
+    <ul className="gap-3 sm:grid sm:grid-cols-2 xl:grid-cols-4 mt-5 ">
       {list.map((el: Elem) => (
-        <li className="border p-5 shadow-lg my-2 " key={el.id}>
+        <li
+          className="border p-5 my-2 shadow-[10px_10px_15px_-10px_rgba(0,0,0,0.3)]"
+          key={el.id}
+        >
           <GradientCardList
             name={el.name}
             colorStart={el.start}
